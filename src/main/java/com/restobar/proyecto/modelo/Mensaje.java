d39da546e4e1c3551a357dd4f4419e56bc0ae106
dimadosu <doloriertdiego010@gmail.com>
2022-11-15 23:04:39 -0500

    Haciendo comit

--- src/main/java/com/restobar/proyecto/modelo/Mensaje.java
+++ src/main/java/com/restobar/proyecto/modelo/Mensaje.java
@@ -1,4 +1,54 @@
 package com.restobar.proyecto.modelo;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mensajes")
 public class Mensaje {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100)
+    private String nombre;
+
+    @Column(length = 100)
+    private String email;
+
+
+    private String mensaje;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
 }
